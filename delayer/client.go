d52b327e154189fe6f590d439987d6009df46183
Jian Liu <onanying@126.com>
2018-10-26 14:37:31 +0800

    优化

--- delayer/client.go
+++ delayer/client.go
@@ -8,8 +8,8 @@ import (
 
 // 键名
 const (
-	KEY_JOP_POOL       = "delayer:jop_pool"
-	PREFIX_JOP_BUCKET  = "delayer:jop_bucket:"
+	KEY_JOB_POOL       = "delayer:job_pool"
+	PREFIX_JOB_BUCKET  = "delayer:job_bucket:"
 	PREFIX_READY_QUEUE = "delayer:ready_queue:"
 )
 
@@ -53,14 +53,21 @@ func (p *Client) Push(message Message, delayTime int, readyMaxLifetime int) (boo
 	if !message.Valid() {
 		return false, errors.New("Invalid message.");
 	}
-	// 增加
+	// 执行事务
 	p.Conn.Send("MULTI")
-	p.Conn.Send("HMSET", PREFIX_JOP_BUCKET+message.ID, "topic", message.Topic, "body", message.Body)
-	p.Conn.Send("EXPIRE", PREFIX_JOP_BUCKET+message.ID, delayTime+readyMaxLifetime)
-	p.Conn.Send("ZADD", KEY_JOP_POOL, time.Now().Unix()+int64(delayTime), message.ID)
-	_, err := p.Conn.Do("EXEC")
+	p.Conn.Send("HMSET", PREFIX_JOB_BUCKET+message.ID, "topic", message.Topic, "body", message.Body)
+	p.Conn.Send("EXPIRE", PREFIX_JOB_BUCKET+message.ID, delayTime+readyMaxLifetime)
+	p.Conn.Send("ZADD", KEY_JOB_POOL, time.Now().Unix()+int64(delayTime), message.ID)
+	values, err := redis.Values(p.Conn.Do("EXEC"))
 	if err != nil {
 		return false, err
+	}
+	// 事务结果处理
+	v := values[0].(string)
+	v1 := values[1].(int64)
+	v2 := values[2].(int64)
+	if v != "OK" || v1 == 0 || v2 == 0 {
+		return false, nil
 	}
 	// 返回
 	return true, nil
@@ -69,17 +76,17 @@ func (p *Client) Push(message Message, delayTime int, readyMaxLifetime int) (boo
 // 取出任务
 func (p *Client) Pop(topic string) (*Message, error) {
 	id, err := redis.String(p.Conn.Do("RPOP", PREFIX_READY_QUEUE+topic))
-	if err != nil || id == "" {
+	if err != nil {
 		return nil, err
 	}
-	result, err := redis.StringMap(p.Conn.Do("HGETALL", PREFIX_JOP_BUCKET+id))
+	result, err := redis.StringMap(p.Conn.Do("HGETALL", PREFIX_JOB_BUCKET+id))
 	if err != nil {
 		return nil, err
 	}
 	if result["topic"] == "" || result["body"] == "" {
-		return nil, errors.New("")
+		return nil, errors.New("Job bucket has expired or is incomplete")
 	}
-	err = p.Conn.Send("DEL", PREFIX_JOP_BUCKET+id)
+	err = p.Conn.Send("DEL", PREFIX_JOB_BUCKET+id)
 	if err != nil {
 		return nil, err
 	}
@@ -94,17 +101,17 @@ func (p *Client) Pop(topic string) (*Message, error) {
 // 阻塞取出任务
 func (p *Client) BPop(topic string, timeout int) (*Message, error) {
 	id, err := redis.String(p.Conn.Do("BRPOP", PREFIX_READY_QUEUE+topic, timeout))
-	if err != nil || id == "" {
+	if err != nil {
 		return nil, err
 	}
-	result, err := redis.StringMap(p.Conn.Do("HGETALL", PREFIX_JOP_BUCKET+id))
+	result, err := redis.StringMap(p.Conn.Do("HGETALL", PREFIX_JOB_BUCKET+id))
 	if err != nil {
 		return nil, err
 	}
 	if result["topic"] == "" || result["body"] == "" {
-		return nil, errors.New("")
+		return nil, errors.New("Job bucket has expired or is incomplete")
 	}
-	err = p.Conn.Send("DEL", PREFIX_JOP_BUCKET+id)
+	err = p.Conn.Send("DEL", PREFIX_JOB_BUCKET+id)
 	if err != nil {
 		return nil, err
 	}
@@ -118,13 +125,15 @@ func (p *Client) BPop(topic string, timeout int) (*Message, error) {
 
 // 移除任务
 func (p *Client) Remove(id string) (bool, error) {
+	// 执行事务
 	p.Conn.Send("MULTI")
-	p.Conn.Send("ZREM", KEY_JOP_POOL, id)
-	p.Conn.Send("DEL", PREFIX_JOP_BUCKET+id)
+	p.Conn.Send("ZREM", KEY_JOB_POOL, id)
+	p.Conn.Send("DEL", PREFIX_JOB_BUCKET+id)
 	values, err := redis.Values(p.Conn.Do("EXEC"))
 	if err != nil {
 		return false, err
 	}
+	// 事务结果处理
 	v := values[0].(int64)
 	v1 := values[1].(int64)
 	if v == 0 || v1 == 0 {
